87882954bbd5926daed293ade431d9debd391dcb
Thiemann423 <familie.thiemann@gmx.de>
2025-06-05 09:01:01 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,1 @@
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('reparatur-app').then(function(cache) {
-      return cache.addAll([
-        './index.html',
-        './manifest.json'
-      ]);
-    })
-  );
-});
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+// Dummy
