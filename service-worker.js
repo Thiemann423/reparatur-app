150487b88a05ceb3f889fbef839016a9c00301b8
Thiemann423 <familie.thiemann@gmx.de>
2025-06-05 11:04:22 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,56 @@
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('reparatur-app').then(function(cache) {
-      return cache.addAll([
-        './index.html',
-        './manifest.json'
-      ]);
+const CACHE_NAME = 'reparatur-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
+// Beim Installieren direkt alle Dateien cachen
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Alte Caches beim Aktivieren entfernen
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+
+// Fetch: immer zuerst online versuchen, dann Fallback auf Cache
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function(response) {
+        return caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(function() {
+        return caches.match(event.request);
+      })
   );
+});
+
+// Update-Erkennung bei neuer Version
+self.addEventListener('message', function (event) {
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
