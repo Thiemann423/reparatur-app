7b565ef43a9e138b82688a1677232f5b49c1c9bc
Thiemann423 <familie.thiemann@gmx.de>
2025-06-10 09:10:46 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,11 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(cacheNames.map(cache => caches.delete(cache)));
+    }).then(() => self.clients.claim())
+  );
 });
 self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request));
